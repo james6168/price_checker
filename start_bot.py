790fa6e4fba5abcda3471e07e59ad28aa193870a
james6168 <95910014+james6168@users.noreply.github.com>
2022-11-07 17:10:03 +0600

    Now all smartphone brands could be show partially

--- start_bot.py
+++ start_bot.py
@@ -1,13 +1,17 @@
-import time
 
 import telebot
+from telebot.types import ReplyKeyboardRemove
+
 from shop_parser import *
 from decouple import config
 from telebot import types
 
 
 bot = telebot.TeleBot(config("TOKEN_BOT"))
-
+list_current_smartphones = []
+continue_button_xiaomi = types.KeyboardButton("Далее")
+stop_button_xiaomi = types.KeyboardButton("Стоп")
+render_counter = 0
 
 @bot.message_handler(commands=["start"])
 def greet_user_and_show_brands(message):
@@ -35,39 +39,71 @@ def greet_user_and_show_brands(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def get_brand_data_and_return_info(call):
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    continue_button = types.InlineKeyboardButton("Далее", callback_data="continue_render")
-    stop_button = types.InlineKeyboardButton("Стоп", callback_data="stop_render")
-    markup.add(continue_button, stop_button)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(continue_button_xiaomi, stop_button_xiaomi)
+    global list_current_smartphones
+
     if call.data == "brand_xiaomi":
-        xiaomi_smartphones = get_smartphones_info_telefon_kg(get_smartphones_list_html("https://telefon.kg/smartphone/xiaomi_smartphones", HEADERS))
-        for each_smartphone in xiaomi_smartphones:
-            bot.send_message(call.message.chat.id, f"{each_smartphone}")
+        list_current_smartphones = get_smartphones_info_telefon_kg(get_smartphones_list_html("https://telefon.kg/smartphone/xiaomi_smartphones", HEADERS))
+        for i in range(0, 5):
+            bot.send_message(call.message.chat.id, f"{list_current_smartphones[i]}")
+        bot.send_message(call.message.chat.id, "Показать другие смартфоны?", reply_markup=markup)
+
     elif call.data == "brand_samsung":
-        realme_smartphones = get_smartphones_info_telefon_kg(
+        list_current_smartphones = get_smartphones_info_telefon_kg(
             get_smartphones_list_html("https://telefon.kg/smartphone/samsung_smartphones", HEADERS))
-        for each_smartphone in realme_smartphones:
-            bot.send_message(call.message.chat.id, f"{each_smartphone}")
+        for i in range(0, 5):
+            bot.send_message(call.message.chat.id, f"{list_current_smartphones[i]}")
+        bot.send_message(call.message.chat.id, "Показать другие смартфоны?", reply_markup=markup)
+
     elif call.data == "brand_realme":
-        realme_smartphones = get_smartphones_info_telefon_kg(
+        list_current_smartphones = get_smartphones_info_telefon_kg(
             get_smartphones_list_html("https://telefon.kg/smartphone/smartfony-oppo", HEADERS))
-        for each_smartphone in realme_smartphones:
-            bot.send_message(call.message.chat.id, f"{each_smartphone}")
+        for i in range(0, 5):
+            bot.send_message(call.message.chat.id, f"{list_current_smartphones[i]}")
+        bot.send_message(call.message.chat.id, "Показать другие смартфоны?", reply_markup=markup)
+
     elif call.data == "brand_google":
-        google_smartphones = get_smartphones_info_telefon_kg(
+        list_current_smartphones = get_smartphones_info_telefon_kg(
             get_smartphones_list_html("https://telefon.kg/smartphone/google-pixel", HEADERS))
-        for each_smartphone in google_smartphones:
-            bot.send_message(call.message.chat.id, f"{each_smartphone}")
+        for i in range(0, 5):
+            bot.send_message(call.message.chat.id, f"{list_current_smartphones[i]}")
+        bot.send_message(call.message.chat.id, "Показать другие смартфоны?", reply_markup=markup)
+
     elif call.data == "brand_apple":
-        apple_smartphones = get_smartphones_info_telefon_kg(
+        list_current_smartphones = get_smartphones_info_telefon_kg(
             get_smartphones_list_html("https://telefon.kg/smartphone/apple_smartphones", HEADERS))
-        for each_smartphone in apple_smartphones:
-            bot.send_message(call.message.chat.id, f"{each_smartphone}")
+        for i in range(0, 5):
+            bot.send_message(call.message.chat.id, f"{list_current_smartphones[i]}")
+        bot.send_message(call.message.chat.id, "Показать другие смартфоны?", reply_markup=markup)
+
     elif call.data == "brand_oneplus":
-        oneplus_smartphones = get_smartphones_info_telefon_kg(
+        list_current_smartphones = get_smartphones_info_telefon_kg(
             get_smartphones_list_html("https://telefon.kg/smartphone/oneplus_smartphones", HEADERS))
-        for each_smartphone in oneplus_smartphones:
-            bot.send_message(call.message.chat.id, f"{each_smartphone}")
+        for i in range(0, 5):
+            bot.send_message(call.message.chat.id, f"{list_current_smartphones[i]}")
+        bot.send_message(call.message.chat.id, "Показать другие смартфоны?", reply_markup=markup)
+
+
+@bot.message_handler(content_types=["text"])
+def get_number_of_render_and_continue_render_or_break(message):
+    global list_current_smartphones
+    global render_counter
+    if message.text.lower() == "далее":
+        for i in range(render_counter, render_counter + 6):
+            if render_counter >= len(list_current_smartphones):
+                render_counter = 0
+                list_current_smartphones.clear()
+                bot.send_message(message.chat.id, "Все смартфоны были отображены", reply_markup=ReplyKeyboardRemove())
+                break
+            bot.send_message(message.chat.id, f"{list_current_smartphones[i]}")
+            render_counter += 1
+    elif message.text.lower() == "стоп":
+        render_counter = 0
+        list_current_smartphones.clear()
+        bot.send_message(message.chat.id, "Показ смартфонов приостановлен", reply_markup=ReplyKeyboardRemove())
+
+
 
 
 bot.polling()
